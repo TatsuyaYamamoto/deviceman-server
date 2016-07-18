33b574f10624ecb61ccd0ff71a3dfeba08193e1f
TatsuyaYamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-07-18 18:10:09 +0900

    impl view

--- src/main/resources/static/jsx/main.jsx
+++ src/main/resources/static/jsx/main.jsx
@@ -126,6 +126,174 @@ var CurrentCheckoutTableHeader = React.createClass({
 
 
 /*********************************************
+ * user component
+ */
+
+
+var UserListBox = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        var self = this;
+        apiclient.getUsers(function(res){
+            self.setState({data: res.body.users});
+        });
+    },
+    render: function() {
+        return (
+            <div className="userListBox">
+                <h1>User List</h1>
+                <table>
+                    <UserTableHeader />
+                    <UserTableBody data={this.state.data} />
+                </table>
+            </div>
+        );
+    }
+});
+
+var UserTableHeader = React.createClass({
+    render: function() {
+        return (
+            <thead className="LogTableHeader">
+            <tr>
+                <th>ユーザーID</th>
+                <th>ユーザー名</th>
+                <th>アドレス</th>
+                <th>QRコード</th>
+            </tr>
+            </thead>
+        );
+    }
+});
+
+var UserTableBody = React.createClass({
+    render: function() {
+        var userNodes = this.props.data.map(
+            (user)=>{
+                var qrURL = "http://chart.apis.google.com/chart?chs=150x150&cht=qr&chl=" + user.id;
+                return (
+                    <tr
+                        className="logItem"
+                        key={user.id}　>
+                        <td className="userId">{user.id}</td>
+                        <td className="userName">{user.name}</td>
+                        <td className="address">{user.address}</td>
+                        <td className="qrCode">
+                            <a href={qrURL}>
+                                <img src={qrURL}></img>
+                            </a>
+                        </td>
+                    </tr>
+                );
+            });
+
+        return (
+            <tbody className="userTableBody">
+            {userNodes}
+            </tbody>
+        );
+    }
+});
+
+
+var UserCreateForm = React.createClass({
+    render: function() {
+        return (
+            <div className="userCreateForm">
+                <h1>User Create Form</h1>
+                <form>
+                    <input type="text" placeholder="employee ID" />
+                    <input type="text" placeholder="your name" />
+                    <input type="text" placeholder="address" />
+                    <input type="password" placeholder="password" />
+                    <input type="submit" value="Post" />
+                </form>
+            </div>
+        );
+    }
+});
+
+
+
+
+/*********************************************
+ * device component
+ */
+
+
+var DeviceListBox = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentDidMount: function() {
+        var self = this;
+        apiclient.getDevices(function(res){
+            self.setState({data: res.body.devices});
+        });
+    },
+    render: function() {
+        return (
+            <div className="deviceListBox">
+                <h1>Device List</h1>
+                <table>
+                    <DeviceTableHeader />
+                    <DeviceTableBody data={this.state.data} />
+                </table>
+            </div>
+        );
+    }
+});
+
+var DeviceTableHeader = React.createClass({
+    render: function() {
+        return (
+            <thead className="deviceTableHeader">
+            <tr>
+                <th>ユーザーID</th>
+                <th>ユーザー名</th>
+                <th>QRコード</th>
+            </tr>
+            </thead>
+        );
+    }
+});
+
+var DeviceTableBody = React.createClass({
+    render: function() {
+        var deviceNodes = this.props.data.map(
+            (device)=>{
+                var qrURL = "http://chart.apis.google.com/chart?chs=150x150&cht=qr&chl=" + device.id;
+                return (
+                    <tr
+                        className="logItem"
+                        key={device.id}　>
+                        <td className="deviceId">{device.id}</td>
+                        <td className="deviceName">{device.name}</td>
+                        <td className="qrCode">
+                            <a href={qrURL}>
+                                <img src={qrURL}></img>
+                            </a>
+                        </td>
+                    </tr>
+                );
+            });
+
+        return (
+            <tbody className="deviceTableBody">
+            {deviceNodes}
+            </tbody>
+        );
+    }
+});
+
+
+
+
+
+
+/*********************************************
  * root content component
  */
 
@@ -135,6 +303,9 @@ var Content = React.createClass({
     render: function() {
         return (
             <div className="content">
+                <UserCreateForm />
+                <UserListBox />
+                <DeviceListBox />
                 <CurrentCheckout />
                 <CheckoutLog />
             </div>
