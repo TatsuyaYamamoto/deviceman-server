88871798b081b9cb1fd39833229878593ebd4e96
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-10-03 14:48:30 +0900

    impl invalid request param exception and handler

---
+++ src/main/java/jp/co/fujixerox/nbd/controller/ExceptionHandler.java
@@ -0,0 +1,53 @@
+package jp.co.fujixerox.nbd.controller;
+
+import jp.co.fujixerox.nbd.exception.InvalidRequestException;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ControllerクラスがthrowするExceptionのレスポンスを定義する
+ */
+@ControllerAdvice
+@Order(0)
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+    private class Response {
+        @Getter
+        @Setter
+        private List<Map> errors;
+    }
+
+    /**
+     * BeanValidationの結果throwされたInvalidRequestExceptionをハンドリングする
+     *
+     * @param e
+     * @return
+     */
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = InvalidRequestException.class)
+    protected ResponseEntity<Object> handleInvalidRequest(InvalidRequestException e) {
+        List<Map> errors = new ArrayList<Map>();
+        for(FieldError fieldError: e.getErrors().getFieldErrors()){
+            Map<String, Object> error = new HashMap<String, Object>();
+            error.put("message", fieldError.getDefaultMessage());
+            error.put("field", fieldError.getField());
+            error.put("rejected_value", fieldError.getRejectedValue());
+
+            errors.add(error);
+        }
+
+        Response response = new Response();
+        response.setErrors(errors);
+
+        return new ResponseEntity(response , HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/jp/co/fujixerox/nbd/exception/InvalidRequestException.java
@@ -0,0 +1,14 @@
+package jp.co.fujixerox.nbd.exception;
+
+import lombok.Getter;
+import org.springframework.validation.Errors;
+
+public class InvalidRequestException extends RuntimeException {
+    @Getter
+    private Errors errors;
+
+    public InvalidRequestException(String message, Errors errors) {
+        super(message);
+        this.errors = errors;
+    }
+}
