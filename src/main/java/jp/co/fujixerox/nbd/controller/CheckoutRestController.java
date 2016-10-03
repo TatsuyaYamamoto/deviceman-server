6921f26fa03e66d36a1c2882c4a2f6761d0d8a33
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2016-10-03 15:46:08 +0900

    add javadoc

--- src/main/java/jp/co/fujixerox/nbd/controller/CheckoutRestController.java
+++ src/main/java/jp/co/fujixerox/nbd/controller/CheckoutRestController.java
@@ -1,22 +1,17 @@
 package jp.co.fujixerox.nbd.controller;
 
-import jp.co.fujixerox.nbd.ApplicationException;
-import jp.co.fujixerox.nbd.controller.form.CreateDeviceForm;
 import jp.co.fujixerox.nbd.domain.model.CheckOut;
-import jp.co.fujixerox.nbd.domain.model.Device;
 import jp.co.fujixerox.nbd.domain.model.Log;
 import jp.co.fujixerox.nbd.domain.repository.CheckOutRepository;
 import jp.co.fujixerox.nbd.domain.repository.LogRepository;
-import jp.co.fujixerox.nbd.domain.service.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +26,12 @@ public class CheckoutRestController {
     @Autowired
     LogRepository logRepo;
 
+    /**
+     * 現在の貸出情報を取得する
+     * 対象は貸出中の端末のみ
+     *
+     * @return
+     */
     @RequestMapping(
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -43,6 +44,12 @@ public class CheckoutRestController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * 貸出履歴を取得する
+     * 対象は返却済みの端末のみ
+     *
+     * @return
+     */
     @RequestMapping(
             value = "/log",
             method = RequestMethod.GET,
