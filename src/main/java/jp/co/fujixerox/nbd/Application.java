3d9722c498501747c5a9c09808bba0bc2f49c31c
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-01-14 01:47:51 +0900

    move deprecate class

--- src/main/java/jp/co/fujixerox/nbd/Application.java
+++ src/main/java/jp/co/fujixerox/nbd/Application.java
@@ -4,7 +4,7 @@ import jp.co.fujixerox.nbd.controller.HttpLogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
